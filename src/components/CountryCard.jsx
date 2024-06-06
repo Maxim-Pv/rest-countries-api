0386318274c7e2736d931d7541ad68a5943988e4
Maksim Pavlov <maxim.paw2010@yandex.ru>
2024-06-07 00:02:46 +0300

    complete

--- src/components/CountryCard.jsx
+++ src/components/CountryCard.jsx
@@ -3,13 +3,13 @@ import { Link } from 'react-router-dom'
 
 const CountryCard = ({ country }) => {
   return (
-    <Link to={`/country/${country.name}`}>
+    <Link className='country-link' to={`/country/${country.name}`}>
       <div className='country-card'>
         <div className='flag-container'>
           <img className='flag' src={country.flags.svg} alt="flag" />
         </div>
         <div className='country-info'>
-          <h3>{country.name}</h3>
+          <h3 className='country-name'>{country.name}</h3>
           <span>
             <strong>Population: </strong>
             {country.population}
--- src/components/CountryList.jsx
+++ src/components/CountryList.jsx
@@ -15,7 +15,25 @@ const CountryList = ({ searchCountry, region }) => {
         country.name.toLowerCase().includes(searchCountry) &&
         (region === '' || country.region.toLowerCase().includes(region))
       )
-    })
+    });
+
+    const priorityOrder = ['Germany', 'United States of America', 'Brazil', 'Iceland'];
+
+    filteredCountries.sort((a, b) => {
+      const indexA = priorityOrder.indexOf(a.name);
+      const indexB = priorityOrder.indexOf(b.name);
+      if (indexA === -1 && indexB === -1) {
+        return a.name.localeCompare(b.name);
+      }
+      if (indexA === -1) {
+        return 1
+      }
+      if (indexB === -1) {
+        return -1
+      }
+      return indexA - indexB
+    });
+
     setCountries(filteredCountries)
   }, [searchCountry, region])
 
